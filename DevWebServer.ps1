2e00f5380ab50ebd2930c718ab40b3856557a3e3
Angelo <a0920217700@gmail.com>
2022-12-14 19:53:06 +0800

    移除defs、devwebserver改版

--- DevWebServer.ps1
+++ DevWebServer.ps1
@@ -1,19 +1,23 @@
 
+
 Set-Location -Path $PSScriptRoot
 
 for ($i = 5670; $i -lt 5674; $i++) {
-    $result = Test-NetConnection "127.0.0.1" -Port $i
-    if ($result.TcpTestSucceeded -eq $false) {
-        $Port = $i
-        break
-    }
+	$result = Test-NetConnection "127.0.0.1" -Port $i
+	if ($result.TcpTestSucceeded -eq $false) {
+		$Port = $i
+		break
+	}
 }
-
 $localAdd = "http://127.0.0.1:" + $Port
+$chromeBinDir = "D:\ProgramPortable\ChromePortable64\chrome.exe"
+$chromeuserDir = "--user-data-dir=D:\ChromeUser\kkkendy"
+Start-Process -FilePath $chromeBinDir -ArgumentList $localAdd, $chromeuserDir
 
-# D:\ProgramPortable\ChromePortable64\ChromePortable.exe $localAdd
-D:\ProgramPortable\ChromePortable64\App\Chrome64\chrome.exe --user-data-dir=D:\\ChromeUser $localAdd
+# code .
+# github
 
-# explorer.exe $pwd\public\assets
+explorer.exe $pwd\assets
 http-server -s -p $Port
+
 
